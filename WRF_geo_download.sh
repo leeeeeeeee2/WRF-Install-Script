e7ad25f5c13b4d1e1e4dbe09002db1d5aca3dcc6
LeeQQQQQQQ <37931523+leeeeeeeee2@users.noreply.github.com>
2022-01-26 20:43:40 +0800

    Create WRF_geo_download.sh
    download data

---
+++ WRF_geo_download.sh
@@ -0,0 +1,24 @@
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/albedo_modis.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/clayfrac_5m.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/erod.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/greenfrac.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/lai_modis_10m.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/lake_depth.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/maxsnowalb.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/modis_landuse_20class_30s_with_lakes.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/nlcd2011_can_ll_9s.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/nlcd2011_imp_ll_9s.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/NUDAPT44_1KM.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/orogwd_10m.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/sandfrac_5m.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/soiltemp_1deg.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/soiltype_bot_5m.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/soiltype_bot_30s.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/soiltype_top_5m.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/soiltype_bot_30s.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/topo_gmted2010_30s.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/topo_gmted2010_5m.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/varsso_10m.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/soiltype_top_10m.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/soiltype_bot_10m.tar.bz2 && tar jxvf soiltype_bot_10m.tar.bz2
+wget https://www2.mmm.ucar.edu/wrf/src/wps_files/greenfrac_fpar_modis.tar.bz2 && tar jxvf greenfrac_fpar_modis.tar.bz2
